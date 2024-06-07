31097a1bc586e377b6afaf90f619bb07d1a641f5
Rohan Sadhukhan <68492239+rohansadhukhan65@users.noreply.github.com>
2024-06-07 18:31:09 +0530

    the-daisy-dreams

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
-import "@/styles/globals.css";
+import "@/styles/main.scss";
 import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
+
+const pop = Poppins({ subsets:['latin'], weight: ["400", "500", "700"] });
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <main className={pop.className}> <Component  {...pageProps} /></main>;
 }
